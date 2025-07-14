9b518c42ee8ebe6705608d091ec67450c13551e3
AlexandraFelecan <ale.neaga25@gmail.com>
2025-07-14 19:33:32 +0300

    Create Contor.java

---
+++ src/tema_14_07/Contor.java
@@ -0,0 +1,9 @@
+package tema_14_07;
+
+public class Contor {
+
+  /* Pentru exercitiul :
+  Creează o clasă Contor cu un atribut static int totalObiecte. În fiecare constructor, crește valoarea totală.
+  Creează 3 obiecte și afișează totalul.  > Mai exista deja o clasa creata in src
+   */
+}
