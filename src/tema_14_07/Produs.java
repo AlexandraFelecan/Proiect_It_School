f2b37423088d38cdeed54bb700af9629abcb9a9f
AlexandraFelecan <ale.neaga25@gmail.com>
2025-07-14 20:26:41 +0300

    Update Produs.java

--- src/tema_14_07/Produs.java
+++ src/tema_14_07/Produs.java
@@ -1,5 +1,5 @@
 package tema_14_07;
-
+// Creează o clasă Produs cu final String codProdus, inițializat în constructor. Creează și o metodă afiseazaDetalii(). În main(), încearcă să modifici codul (trebuie să apară eroare).
 public class Produs {
 
     final String codProdus;
