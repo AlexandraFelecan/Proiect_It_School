b1f969c1437b17b1b7983153830be757812a7c13
AlexandraFelecan <ale.neaga25@gmail.com>
2025-07-14 19:33:38 +0300

    Create Vocale.java

---
+++ src/tema_14_07/Vocale.java
@@ -0,0 +1,26 @@
+package tema_14_07;
+
+public class Vocale {
+
+    public static int numaraVocale(String text) {
+        int count = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            switch (c) {
+                case 'a': case 'e': case 'i': case 'o': case 'u':
+                case 'A': case 'E': case 'I': case 'O': case 'U':
+                    count++;
+                    break;
+                default:
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String propozitie = "Ana are mere";
+        int nrVocale = numaraVocale(propozitie);
+        System.out.println("Număr de vocale: " + nrVocale);
+    }
+}
+
