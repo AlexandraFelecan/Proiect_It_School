db9822da70d047ec3ae7a3bccf655908d11bc1e4
AlexandraFelecan <ale.neaga25@gmail.com>
2025-07-14 20:26:55 +0300

    Update Factura.java

--- src/tema_14_07/Factura.java
+++ src/tema_14_07/Factura.java
@@ -2,8 +2,9 @@ package tema_14_07;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
+
+// Creează o clasă Factura cu atributele nrFactura, valoare, data. Creează un constructor care setează aceste valori și o metodă afiseazaFactura() care afișează detaliile.
 
 public class Factura {
 
@@ -11,20 +12,20 @@ public class Factura {
     double valoare;
     LocalDate data;
 
-    public Factura (String nr, double val, LocalDate d){
+    public Factura(String nr, double val, LocalDate d) {
         nrFactura = nr;
         valoare = val;
         data = d;
     }
 
-    public void afiseazaFactura(){
+    public void afiseazaFactura() {
         Date date = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
         String formattedDate = sdf.format(date);
-        System.out.println("tema_14_07.Factura cu nr: " + nrFactura + " in valoare de: " + valoare +" lei din data de " + formattedDate);
+        System.out.println("tema_14_07.Factura cu nr: " + nrFactura + " in valoare de: " + valoare + " lei din data de " + formattedDate);
     }
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Factura f = new Factura("F124", 250.55, LocalDate.now());
         f.afiseazaFactura();
     }
