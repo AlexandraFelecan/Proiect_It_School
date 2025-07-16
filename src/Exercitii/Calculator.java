b81f532c76b2c8f6d24d8e198d60cbd87fe6cf42
AlexandraFelecan <ale.neaga25@gmail.com>
2025-07-16 18:16:38 +0300

    Create Calculator.java

---
+++ src/Exercitii/Calculator.java
@@ -0,0 +1,22 @@
+package Exercitii;
+
+public class Calculator {
+    // Metoda pentru adunare int
+    int aduna(int a, int b) {
+        return a + b;
+    }
+
+    double aduna(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        int rezultatInt = calc.aduna(3, 4);
+        double rezultatDouble = calc.aduna(2.5, 3.7);
+
+        System.out.println("Rezultat int: " + rezultatInt);
+        System.out.println("Rezultat double: " + rezultatDouble);
+    }
+}
