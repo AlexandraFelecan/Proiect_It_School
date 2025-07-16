f1aeb364ba0e7d02da08f67b2e2c3d19491ee6ce
AlexandraFelecan <ale.neaga25@gmail.com>
2025-07-16 18:16:53 +0300

    Create Constante.java

---
+++ src/Exercitii/Constante.java
@@ -0,0 +1,13 @@
+package Exercitii;
+
+public class Constante {
+    public static final double PI = 3.14159;
+
+    public static void afiseazaPI(){
+        System.out.println("Valoarea lui PI este: " + PI);
+    }
+
+    public static void main(String[]args){
+        Constante.afiseazaPI();
+    }
+}
