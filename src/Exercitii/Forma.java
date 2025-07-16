19dc78d070d9bfef44590667cb9f1dac02b2e5d2
AlexandraFelecan <ale.neaga25@gmail.com>
2025-07-16 18:17:03 +0300

    Create Forma.java

---
+++ src/Exercitii/Forma.java
@@ -0,0 +1,12 @@
+package Exercitii;
+
+abstract class Forma {
+
+    abstract void deseneaza();
+
+    class Cerc extends Forma{
+        void deseneaza() {
+            System.out.println("Deseneaza un cerc");
+        }
+    }
+}
