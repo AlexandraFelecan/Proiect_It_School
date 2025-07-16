71357a60b36a26a156ee96784468b0f2b9f71724
AlexandraFelecan <ale.neaga25@gmail.com>
2025-07-16 18:17:12 +0300

    Update Contor.java

--- src/tema_14_07/Contor.java
+++ src/tema_14_07/Contor.java
@@ -3,7 +3,7 @@ package tema_14_07;
 public class Contor {
 
   /* Pentru exercitiul :
-  Creează o clasă Contor cu un atribut static int totalObiecte. În fiecare constructor, crește valoarea totală.
+  Creează o clasă Exercitii.Contor cu un atribut static int totalObiecte. În fiecare constructor, crește valoarea totală.
   Creează 3 obiecte și afișează totalul.  > Mai exista deja o clasa creata in src
    */
 }
