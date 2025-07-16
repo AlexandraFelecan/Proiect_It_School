bdc2728d2c26d436767eea815352c0b3e21409a2
AlexandraFelecan <ale.neaga25@gmail.com>
2025-07-16 18:41:18 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
-
+import java.time.LocalDate;
 import java.util.Scanner;
 
-
 public class Main {
-
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introdu numele tau: ");
-        String nume = sc.nextLine();
-        System.out.println("Introdu varsta: ");
-        int varsta = sc.nextInt();
-        System.out.println("Salut, " + nume + " ai " + varsta +" ani.");
-        sc.close();
     }
 }
 
