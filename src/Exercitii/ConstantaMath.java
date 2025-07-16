1edebe1a4a739607e739c239554d16134873e426
AlexandraFelecan <ale.neaga25@gmail.com>
2025-07-16 18:16:49 +0300

    Create ConstantaMath.java

---
+++ src/Exercitii/ConstantaMath.java
@@ -0,0 +1,14 @@
+package Exercitii;
+
+public class ConstantaMath {
+    final double PI = 3.14;
+
+    public static void main(String[] args) {
+        ConstantaMath cm = new ConstantaMath();
+
+//       cm.PI = 3.1415;
+
+        System.out.println("Valoarea lui PI este: " + cm.PI);
+    }
+}
+
