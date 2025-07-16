aa452342639779be12ae7ba3ef026efb8fb9025e
AlexandraFelecan <ale.neaga25@gmail.com>
2025-07-16 18:17:19 +0300

    Create Utilizator.java

---
+++ src/Exercitii/Utilizator.java
@@ -0,0 +1,11 @@
+package Exercitii;
+
+public class Utilizator {
+    private String parola;
+
+    // Constructor
+    public Utilizator(String p) {
+        parola = p;
+        System.out.println("Parola setată");
+    }
+}
